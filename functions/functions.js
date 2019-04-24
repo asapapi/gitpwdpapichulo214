26404fc60d5c880a098a40201068710353f1e724
Asahi <asahimojica@yahoo.com>
2019-04-24 15:32:26 -0500

    calculator

---
+++ functions/functions.js
@@ -0,0 +1,64 @@
+
+
+var customerTotal = Number(prompt("how much was your meal"));
+var customerTip = Number(prompt("what percentage for the tip"));
+var x = customerTotal*customerTip/100;
+var finalTotal = x + customerTotal;
+
+
+ function calculateGratuity(customerTip, customerTotal){
+    return Number (alert("i recommend this amount " + x));
+}
+
+function finalTotaltwo(finalTotal){
+  return Number( alert('your final amount will be ' + finalTotal) 
+  + finalTotal);
+
+}
+
+// calculateGratuity(customerTotal, customerTip) ;
+
+calculateGratuity();
+finalTotaltwo(finalTotal);
+// debugger;
+
+
+// var customerTotal
+// var ageJohn = calculateAge(1990);
+// var ageAsahi = calculateAge(1995);
+// var ageRae = calculateAge(1996);
+
+// console.log(ageJohn, ageAsahi, ageRae);
+
+// function ageUntilRetirement(year, firstName){
+//     var age = calculateAge(year);
+//     var retirement = 65 - age;
+//     console.log(firstName + " retires in " + retirement + " years ");
+// }
+// ageUntilRetirement(1990, 'john');K
+
+
+
+
+// var billAmount = Number(prompt("How much was your food?"));
+// var tip_percent = Number(prompt("What percent do you want to tip?"));
+// function gratuity(){
+
+
+    
+//      return billAmount * (tip_percent/100);
+
+
+
+    
+// }
+
+// function totalWithGrat(billAmount){
+
+// return gratuity() + billAmount;
+    
+// }
+
+// var total = totalWithGrat(billAmount);
+// alert("We recomend " + gratuity().toFixed("2"));
+// alert("Your total amount with tips added is " + total.toFixed("2"));
